214dcf273b6ae6e940daa664a7802e6ec4cefa4a
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-11-21 08:47:53 +0530

    Navbar is completed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter
@@ -9,6 +10,8 @@ createRoot(document.getElementById('root')).render(
         v7_startTransition: true,
         v7_relativeSplatPath: true,
       }}>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </BrowserRouter>,
 )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex flex-col justify-center items-center text-center my-20'>
+      <div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500'>
+        <p>Best text to image generator</p>
+        <img src={assets.star_icon} alt="" />
+      </div>
+    </div>
   )
 }
 
