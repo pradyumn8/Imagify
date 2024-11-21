2657f6c02daa196562913a219c85f7417794740b
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-11-21 15:03:27 +0530

    Implematation of framer motion on every page

--- client/src/components/GenerateBtn.jsx
+++ client/src/components/GenerateBtn.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { motion } from 'framer-motion'
+
 
 const GenerateBtn = () => {
   return (
-    <div className='pb-16 text-center'>
+    <motion.div 
+    initial={{ opacity: 0.2, y: 100 }}
+    transition={{ duration: 1 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    viewport={{ once: true }}
+    className='pb-16 text-center'>
         <h1 className='text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold text-neutral-800 py-6 md:py-16'>See the magin. Try now</h1>
 
         <button className='inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:scale-1-5 transition-all duration-500'>Generate Images
             <img src={assets.star_group} className='h-6' alt="" />
         </button>
-    </div>
+    </motion.div>
   )
 }
 
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
 
 const Login = () => {
 
     const [state, setState] = useState('Login')
+    const {setShowLogin} = useContext(AppContext)
 
+    useEffect(()=>{
+        document.body.style.overflow = 'hidden'
+
+        return ()=>{
+            document.body.style.overflow = 'unset';
+        }
+    },[])
     return (
         <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
 
             <form className="relative bg-white p-10 rounded-xl text-slate-500">
-                <h1 className='text-center text-2xl text-neutral-700 font-medium'>Sign Up</h1>
+                <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state === 'Sign Up' ? 'Sign Up' : 'Login'}</h1>
                 <p>Welcome back! Please sign in to continue</p>
                 {state !== 'Login' &&
                     <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
